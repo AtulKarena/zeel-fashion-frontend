1ea9d69dfe8e9de8e39ea1c96879410d8bea7539
AtulKarena <atulkarena602@gmail.com>
2026-04-24 10:58:30 +0530

    CHanges in footer file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
               href="/contact"
               className="rounded-full border border-black/10 px-4 py-2 text-sm font-medium transition hover:border-black/30"
             >
-              Contact
+              Contact 
             </Link>
             <Link
               href="/shop"
