31cb95a24910e3be953999554647a49b920b0823
AtulKarena <atulkarena602@gmail.com>
2026-05-11 17:15:34 +0530

    Bug solve

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,9 +4,9 @@ import type { NextRequest } from 'next/server'
 export function proxy(request: NextRequest) {
   const token = request.cookies.get('token')
 console.log('Proxy middleware executed. Token:', token)
-  if (!token) {
+ /*  if (!token) {
     return NextResponse.redirect(new URL('/login', request.url))
-  }
+  } */
 
   return NextResponse.next()
 }
