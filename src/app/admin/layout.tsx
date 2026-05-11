560af5991b960dd822a8d008592959dbf3f3de97
AtulKarena <atulkarena602@gmail.com>
2026-05-11 17:34:37 +0530

    Error Solve

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -15,8 +15,8 @@ export default async function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const token = (await cookies()).get("token");
-console.log("AdminLayout token:", token);
+  const token = (await cookies()).get("token")?.value;
+console.log("AdminLayout token:", (await cookies()).get("token"));
   if (!token) {
     redirect("/login");
   }
