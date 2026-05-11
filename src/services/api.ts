ee71016b015bb9ba34f2b7b88e5a8d35807fb2f7
AtulKarena <atulkarena602@gmail.com>
2026-05-11 12:36:08 +0530

    Add live server url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://zeel-fashion-backend-d53hzz1d7-aks-projects-cb525b83.vercel.app//api",
 });
 
 // Attach JWT token
