7447c5581f91b14d3f56a67c6236c32a01e8154f
AtulKarena <atulkarena602@gmail.com>
2026-05-11 17:13:09 +0530

    Debbuging admin panel

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 export function proxy(request: NextRequest) {
   const token = request.cookies.get('token')
-
+console.log('Proxy middleware executed. Token:', token)
   if (!token) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
