aa827a167b3a4e8223b5ffb30b47702d42c7a3b2
AtulKarena <atulkarena602@gmail.com>
2026-05-13 19:00:41 +0530

    Still trying

--- src/app/admin/orders/_services/order-hooks.ts
+++ src/app/admin/orders/_services/order-hooks.ts
@@ -41,7 +41,7 @@ export const useOrderById = (id: string) => {
     queryKey: ["order", id],
     queryFn: () => orderActions.getOrderById(id),
   });
-  console.log("Fetched order data:", data.data); // Debug log to inspect the fetched order data
+  console.log("Fetched order data:", data); // Debug log to inspect the fetched order data
   return {
     order: data as Order,
     isLoading,
